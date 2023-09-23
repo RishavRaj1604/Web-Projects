578aa16b5e2c9b6561d06bb4056d6eb9c4c04ffa
RishavRaj1604 <143750240+RishavRaj1604@users.noreply.github.com>
2023-09-24 00:28:16 +0530

    Added ParallaxWeb2

---
+++ Parallaxweb2/practical_4.js
@@ -0,0 +1,14 @@
+var x=0;
+var array=Array();
+function createListElement()
+{
+  array[x]=document.getElementById('userInput').value;
+  var e="<hr/">
+  for (var i = 0; i < array.length; i++) {
+    array[i]
+  }(var i=0; i<array.length; i++)
+  {
+    e +="element" + i + "=" +array[i]
+  }
+  document.getElementById('enter').innerHtml=e;
+}
